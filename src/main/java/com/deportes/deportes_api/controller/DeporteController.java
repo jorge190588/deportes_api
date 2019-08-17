14ae62931a7289574a69956cc3902b4c50e8ea1e
jorge190588 <netneill@hotmail.com>
2019-08-17 11:10:50 -0600

    remove unused imports in DeporteController

--- src/main/java/com/deportes/deportes_api/controller/DeporteController.java
+++ src/main/java/com/deportes/deportes_api/controller/DeporteController.java
@@ -1,21 +1,13 @@
 package com.deportes.deportes_api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
