ae389dbe1508da447a7e82cc82871c0b634d03fa
jorge190588 <netneill@hotmail.com>
2019-08-24 13:28:22 -0600

    add error classes

---
+++ src/main/java/com/deportes/deportes_api/error/ErrorCode.java
@@ -0,0 +1,28 @@
+package com.deportes.deportes_api.error;
+
+public enum ErrorCode {
+	GENERIC_ERROR(100,"Generic exception has occurred"),
+	APPLICATION_CONFIG(101,"Error when configuring the application"),
+	ENTITI(101,"Entiti name doesnt exist for the module"),
+	DATABASE_CONNECTION(200,"The connection to the database failed"),
+	DATABASE_TABLE(201,"The connection to the database failed"),
+	REST_QUERY(201,"Custom query"),
+	REST_FIND(300,"Find registers"),
+	REST_CREATE(301,"Create a registrer"),
+	REST_UPDATE(302,"Update a registrer"),
+	REST_DELETE(303,"Delete a registrer");
+	
+	
+	
+	private final int _code;
+	private final String _title;
+
+	ErrorCode(int code, String title) {
+		this._code = code;
+	    this._title = title;
+	}
+
+	public int get_code() { return this._code; }
+	public String get_title() { return this._title; }
+	  
+}
